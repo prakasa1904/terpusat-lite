760e97dd36e101d534a0206bde3c27bc49cdd604
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2017-07-15 15:50:48 +0700

    new config with env configuration

---
+++ server/config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  PORT: process.env.PORT || 4000,
+  __DEV__: process.env.NODE_ENV === 'development',
+  __TEST__: process.env.NODE_ENV === 'test',
+  __PROD__: process.env.NODE_ENV === 'production',
+}
---
+++ server/index.js
@@ -0,0 +1,46 @@
+import path from 'path'
+import webpack from 'webpack'
+import express from 'express'
+import {
+  __DEV__,
+  __TEST__,
+  __PROD__,
+  PORT
+} from './config'
+import webpackConfig from '../build/webpack.config'
+
+const app = express()
+
+//app.get('/', (req, res) => res.end('ok'))
+
+if (__DEV__) {
+  const compiler = webpack(webpackConfig)
+
+  app.use(require('webpack-dev-middleware')(compiler, { // eslint-disable-line global-require
+    publicPath: '/',
+    contentBase: path.resolve('dist'),
+    hot: true,
+    quiet: false,
+    noInfo: false,
+    lazy: false,
+  }))
+  app.use(require('webpack-hot-middleware')(compiler)) // eslint-disable-line global-require
+
+  // Serve static assets from ~/src/static since Webpack is unaware of
+  // these files. This middleware doesn't need to be enabled outside
+  // of development since this directory will be copied into ~/dist
+  // when the application is compiled.
+  app.use(express.static(path.resolve('static')))
+
+  // serve the generated service-worker file
+  app.get('/service-worker.js', (req, res) => {
+    res.sendFile(`path.resolve('dist')}/service-worker.js`)
+  })
+}
+
+if (!PORT) {
+  throw new Error('env PORT is undefined')
+}
+
+app.listen(PORT)
+console.log(`App is now running at http://localhost:${PORT}.`) // eslint-disable-line no-console
